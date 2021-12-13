679c38678021e1a21cfb3e84dfa4cb07e72b2c37
Chris Peterson <cdpeters1@gmail.com>
2021-12-12 23:50:47 -0800

    Add README.md and final script

--- PyPoll.py
+++ PyPoll.py
@@ -1,112 +1,138 @@
-import csv
-import os
-# Path of file to read from
-file_to_load = os.path.join("resources", "election_results.csv")
-# Path of file to write to
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+# -*- coding: UTF-8 -*-
+"""PyPoll Challenge - complete election audit, output results"""
 
-# Total votes for entire election
+import csv
+import pathlib
+
+# Add a variable to load a file from a path
+file_to_load = pathlib.Path("resources", "election_results.csv")
+# Add a variable to save the file to a path
+file_to_save = pathlib.Path("analysis", "election_analysis.txt")
+
+# Initialize a total vote counter
 total_votes = 0
 
-# Candidates and their votes cast
+# Candidate Options and candidate votes
 candidate_options = []
 candidate_votes = {}
 
-# 1: Create a county list and county votes dictionary.
-# Counties and their votes cast
+# 1: Create a county list and county votes dictionary
 county_options = []
 county_votes = {}
 
-# Winning candidate and their vote count/percentage
+# Track the winning candidate, vote count and percentage
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
 
-# 2: Track the largest county and county voter turnout.
-# Largest turnout county and their vote count
+# 2: Track the largest county and county voter turnout
 largest_turnout_county = ""
 largest_turnout_count = 0
 
-# Open the election results and read the file
+# Read and assign data to variables -------------------------------------------
+
+# Open the election results csv and read the file
 with open(file_to_load) as election_data:
-    # Read the file object with the reader function
     file_reader = csv.reader(election_data)
 
     # Read and print the header row
     headers = next(file_reader)
 
     for row in file_reader:
+        # Add to the total vote count
         total_votes += 1
+        # Get the candidate name from each row
         candidate_name = row[2]
-        # 3: Extract the county name from each row.
+        # 3: Extract the county name from each row
         county_name = row[1]
 
+        # If the candidate does not match any existing candidate, add it to
+        # the candidate list
         if candidate_name not in candidate_options:
+            # Initialize the candidate and their vote count
             candidate_options.append(candidate_name)
             candidate_votes[candidate_name] = 0
+        # Add a vote to that candidate's count
         candidate_votes[candidate_name] += 1
 
-        # 4a: Write an if statement that checks that the
-        # county does not match any existing county in the county list.
+        # 4a: Write an if statement that checks that the county does not match
+        # any existing county in the county list
         if county_name not in county_options:
-            # 4b: Add the existing county to the list of counties.
+            # 4b: Add the existing county to the list of counties
             county_options.append(county_name)
-            # 4c: Begin tracking the county's vote count.
+            # 4c: Begin tracking the county's vote count
             county_votes[county_name] = 0
-        # 5: Add a vote to that county's vote count.
+        # 5: Add a vote to that county's vote count
         county_votes[county_name] += 1
 
+# Compute and write results to output file ------------------------------------
+
+# Output to terminal and save the results to our text file
 with open(file_to_save, "w") as txt_file:
+    # Print the final vote count (to terminal)
     election_results = (
         f"\nElection Results\n"
         f"-------------------------\n"
         f"Total Votes: {total_votes:,}\n"
-        f"-------------------------\n"
+        f"-------------------------\n\n"
+        f"County Votes:\n"
     )
     print(election_results, end="")
+
     txt_file.write(election_results)
-
+    # Counties ----------------------------------------------------------------
+    # 6a: Write a for loop to get the county from the county dictionary
     for county_name in county_options:
+        # 6b: Retrieve the county vote count
         votes = county_votes[county_name]
-        vote_percentage = (float(votes) / float(total_votes)) * 100
-
+        # 6c: Calculate the percentage of votes for the county
+        vote_percentage = (votes / float(total_votes)) * 100
+        # 6d: Print the county results to the terminal
         county_results = (
             f"{county_name}: {vote_percentage:.1f}% "
             f"({votes:,})\n"
         )
-        print(county_results)
+        print(county_results, end="")
+        # 6e: Save the county votes to a text file
         txt_file.write(county_results)
-
+        # 6f: Write an if statement to determine the winning county and get its
+        # vote count
         if (votes > largest_turnout_count):
             largest_turnout_count = votes
             largest_turnout_county = county_name
-
+    # 7: Print the county with the largest turnout to the terminal
     largest_turnout_summary = (
-        f"-------------------------\n"
+        f"\n-------------------------\n"
         f"Largest County Turnout: {largest_turnout_county}\n"
         f"-------------------------\n"
     )
     print(largest_turnout_summary)
+    # 8: Save the county with the largest turnout to a text file
     txt_file.write(largest_turnout_summary)
 
-#------------------------------------------------------------------------------
-
+    # Candidates --------------------------------------------------------------
+    # Save the final candidate vote count to the text file
     for candidate_name in candidate_options:
-        votes = candidate_votes[candidate_name]
+        # Retrieve vote count and percentage
+        votes = candidate_votes.get(candidate_name)
         vote_percentage = (float(votes) / float(total_votes)) * 100
 
         candidate_results = (
             f"{candidate_name}: {vote_percentage:.1f}% "
             f"({votes:,})\n"
         )
+        # Print each candidate's voter count and percentage to the terminal
         print(candidate_results)
+        #  Save the candidate results to our text file
         txt_file.write(candidate_results)
 
+        # Determine winning vote count, winning percentage, and candidate
         if (votes > winning_count) and (vote_percentage > winning_percentage):
             winning_count = votes
-            winning_percentage = vote_percentage
             winning_candidate = candidate_name
+            winning_percentage = vote_percentage
 
+    # Print the winning candidate (to terminal)
     winning_candidate_summary = (
         f"-------------------------\n"
         f"Winner: {winning_candidate}\n"
@@ -115,4 +141,6 @@ with open(file_to_save, "w") as txt_file:
         f"-------------------------\n"
     )
     print(winning_candidate_summary)
+
+    # Save the winning candidate's name to the text file
     txt_file.write(winning_candidate_summary)
--- README.md
+++ README.md
@@ -1,1 +1,141 @@
-# election-analysis-python
+# **Automating an Election Audit with Python**
+
+## **Overview of Project**
+Elections, especially on a national level, naturally produce large data sets
+that require the use of automated programs for auditing the results. The
+purpose of this project is to produce an election audit for a 2018 U.S. House
+of Representatives election in Colorado as a case study in how to automate an
+analysis. The goal is to produce a flexible python script that could be adapted
+to analyze new data sets of a similar structure in future elections. The audit
+results will include the vote counts for each candidate as well as voter
+turnout numbers for each county. Output also contains a declaration of the
+winner of the election, along with the county that had the largest voter
+turnout. The raw data is contained in
+[election_results.csv](/resources/election_results.csv), which can be found in
+the resources folder.
+
+## **Analysis and Results**
+The analysis is carried out in the python script ```PyPoll.py``` which is
+roughly split into two parts:
+1. Initialization and Reading Input Data
+1. Writing Output Data
+
+### **Initialization and Reading Input Data**
+The initialization portion included creating variables to hold file paths along
+with other variables necessary for holding candidate and county information.
+Candidates, counties, and vote totals were appended and incremented as
+necessary (via conditional statements) all within a ```with``` block to ensure
+appropriate file handling. This section only contains hard coded information in
+two locations outside of the specific file paths:
+```python
+candidate_name = row[2]
+county_name = row[1]
+```
+In the future, these values can be abstracted away into variables to allow
+more flexibility if the structure of the raw data file changes.
+
+### **Writing Output Data**
+Once the names and counts were gathered, another ```with``` block was used for
+analysis, formatting, and writing to the output file. Two ```for``` loops then
+cycle through each county and candidate to calculate the vote percentages with
+the line:
+```python
+vote_percentage = (votes / float(total_votes)) * 100
+```
+Notice the type conversion of ```total_votes``` to a ```float```. This is to
+ensure the division always results in a ```float``` in case other versions of
+python are used.
+
+To capture the largest county and winning candidate information, the following
+conditional is evaluated at each iteration to see if the appropriate variables
+need to be updated. The conditionals shown are found in separate ```for```
+loops within the script, but are shown together for conciseness (the code that
+is in between is implied by the ellipsis):
+```python
+if (votes > largest_turnout_count):
+    largest_turnout_count = votes
+    largest_turnout_county = county_name
+...
+if (votes > winning_count) and (vote_percentage > winning_percentage):
+    winning_count = votes
+    winning_candidate = candidate_name
+    winning_percentage = vote_percentage
+```
+
+Results, shown in the following section, were then formatted and printed to the
+terminal as well as to the output file
+[election_analysis.txt](/analysis/election_analysis.txt) found in the analysis
+folder.
+
+### **Results**
+The data presented is the terminal output that the script produces. Here are
+the main results of the audit:
+* The total number of votes cast in the election:
+```
+Election Results
+-------------------------
+Total Votes: 369,711
+-------------------------
+```
+
+* The following contains a breakdown of the voting turnout by county (vote
+count in parentheses):
+```
+County Votes:
+Jefferson: 10.5% (38,855)
+Denver: 82.8% (306,055)
+Arapahoe: 6.7% (24,801)
+```
+
+* Thus, the largest voter turnout by county is Denver with 306,055 votes cast:
+```
+-------------------------
+Largest County Turnout: Denver
+-------------------------
+```
+
+* The breakdown of votes for each candidate is as shown (vote count in
+parentheses):
+```
+Charles Casper Stockham: 23.0% (85,213)
+
+Diana DeGette: 73.8% (272,892)
+
+Raymon Anthony Doane: 3.1% (11,606)
+```
+
+* Thus, the winner of the election is Diana DeGette with 73.8% of the total
+vote:
+```
+-------------------------
+Winner: Diana DeGette
+Winning Vote Count: 272,892
+Winning Percentage: 73.8%
+-------------------------
+```
+
+## **Summary**
+The script provided can be used with any election data that follows the same
+format as the raw CSV file, where county and candidate name are the second and
+third values in each row respectively. One way in which the script can be
+restructured to add flexibility is to recast the entire script as a function.
+The inputs to the function can contain information about the structure of the
+raw data file to be read. This would allow election commissions to analyze raw
+data files that are different in structure with the same script as long as the
+structure is specified in the arguments to the function. Something similar to:
+```python
+def election_audit(filepath, county_index, candidate_index)
+```
+Here ```county_index``` and ```candidate_index``` would contain the value of
+the index where those respective names can be found within a row. These indices
+would be used in place of the hard coded values mentioned in the
+**Initialization and Reading Input Data** section. Additional parameters can be
+included as needed.
+
+Another modification that can be made is to add the ability to interact with
+the folder structure of the operating system. Combine this with a loop over a
+folder that holds many election data files and you can produce an output audit
+file with results for each election in the folder. This would allow for all
+elections under that commission to be analyzed with one script, provided the
+raw data files have the same structure as implemented here. These are just two
+modifications among many that show how powerful automating scripts can be.
